43fdabfc968b339a1dbf744b9fe1b5f5523482b3
Darryl Fuller <darrylf@apple.com>
2017-03-11 19:17:20 -0800

    G link on carnie theme.

--- carnie/functions.php
+++ carnie/functions.php
@@ -32,6 +32,9 @@ function carnie_external_links() {
 			<li class="big_link">
 				<a href="http://www.openairorchestra.com"><img src="<?php bloginfo('template_directory'); ?>/images/open_air_society_logo.png" /></a>
 			</li>
+			<li class="big_link">
+                               <a href="http://greenhorn.openairorchestra.com/greenhorn/about/about-the-greenhorn-community-music-project/"><img src="<?php bloginfo('template_directory'); ?>/images/greenhorn_project_logo.png" /></a>
+			</li>
 			<li><a href="http://www.flickr.com/groups/614167@N22/">
 				<img src="<?php bloginfo('template_directory'); ?>/images/flickr.png" />
 				</a></li>
@@ -41,12 +44,12 @@ function carnie_external_links() {
 			<li><a href="http://openairorchestra.blogspot.com/search/label/video">
 				<img src="<?php bloginfo('template_directory'); ?>/images/you_tube_2.png" />
 				</a></li>
-			<li><a href="http://openairorchestra.blogspot.com/">
+                       <li><a href="http://openairorchestra.blogspot.com">
 				<img src="<?php bloginfo('template_directory'); ?>/images/blogger_2.png" />
-</a></li>
+				</a></li>
 			<li><a href="https://twitter.com/TheCarnivalBand">
 				<img src="<?php bloginfo('template_directory'); ?>/images/twitter.png" />
-</a></li>
+			</a></li>
 		</ul>
 	</div>
 
