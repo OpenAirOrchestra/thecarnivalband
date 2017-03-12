11167b11c5c3cf24eecb22da5a60741880be5c29
Darryl Fuller <darrylf@apple.com>
2017-03-11 18:49:32 -0800

    Add Greenhorn "G" logo to top links

--- oao/functions.php
+++ oao/functions.php
@@ -13,6 +13,8 @@ function carnie_external_links_override() {
 			<li class="big_link">
 				<a href="http://www.thecarnivalband.com"><img src="<?php bloginfo('template_directory'); ?>/images/carnival_band_logo.png" /></a>
 			</li>
+			<li class="big_link">
+				<a href="http://greenhorn.openairorchestra.com/greenhorn/about/about-the-greenhorn-community-music-project/"><img src="<?php bloginfo('template_directory'); ?>/images/greenhorn_project_logo.png" /></a>
 			<li><a href="http://www.flickr.com/groups/614167@N22/">
 				<img src="<?php bloginfo('template_directory'); ?>/images/flickr.png" />
 				</a></li>
