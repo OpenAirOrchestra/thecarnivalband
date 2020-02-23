e67ec651eb834d31c8a2d67161a57b08f21ab519
Darryl F <darryl@tzap.ca>
2020-02-23 15:26:16 -0800

    The custom logo

--- header.php
+++ header.php
@@ -28,6 +28,7 @@
 		<div class="site-branding">
 			<?php
 			the_custom_logo();
+			/* For now, disabling showing the web site identity.  The logo does that.
 			if ( is_front_page() && is_home() ) :
 				?>
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
@@ -37,6 +38,7 @@
 				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
 				<?php
 			endif;
+			*/
 			$thecarnivalband_description = get_bloginfo( 'description', 'display' );
 			if ( $thecarnivalband_description || is_customize_preview() ) :
 				?>
